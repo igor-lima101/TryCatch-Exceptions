61dc266d542772e389ac399340cd81273b046490
igor-lima101 <igormaksym96@gmail.com>
2025-09-05 10:06:25 -0300

    feat: test methods Main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,26 @@ public class Main {
     public static void main(String[] args) {
 
 
+
+        BankAccount account = new BankAccount(1, "Sheeva", 200);
+
+        try {
+            account.deposit(100);
+            account.withdraw(50);
+            account.withdraw(300); // InsufficientFunds error
+        } catch (InvalidAmountException e) {
+            System.out.println("Error: " + e.getMessage());
+        } catch (InsufficientFundsException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        try {
+            account.deposit(-10); // InvalidAmount error
+        } catch (InvalidAmountException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+
+        System.out.println(account);
+
     }
 }
